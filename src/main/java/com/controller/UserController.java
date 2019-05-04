fcac53d41fa5c3d71ccbf1e207f357ef42705993
tom_glb <goodMorning_pro@atguigu.com>
2019-05-04 20:27:40 +0800

    test:测试insert

--- src/main/java/com/controller/UserController.java
+++ src/main/java/com/controller/UserController.java
@@ -1,7 +1,9 @@
 package com.controller;
 
 import com.bean.Address;
+import com.bean.User;
 import com.service.AddressService;
+import com.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,6 +18,9 @@ public class UserController {
     @Autowired
     private AddressService addressService;
 
+    @Autowired
+    private UserService userService;
+
     @RequestMapping("/index")
     public String index(Model model){
         // 获取省级城市信息
@@ -29,6 +34,16 @@ public class UserController {
     @ResponseBody
     public List<Address> getCounty(Model model, @RequestParam(value = "parentid", defaultValue = "0") Integer parentid){
         return addressService.getAddressesByParentId(parentid);
+    }
+
+    @RequestMapping(value = "/intoInsert", method = RequestMethod.GET)
+    public String intoInsert(Model model){
+        User user = new User();
+        user.setEmail("123");
+        user.setPassword("123");
+        System.out.println(user);
+        userService.insert(user);
+        return "index";
     }
 
 }
